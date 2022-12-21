e2f393b2172872afd549150fab359fd218163ac3
pplociennik <pplociennik097@gmail.com>
2022-12-21 20:58:54 +0100

    [PPA-22] Sending emails should depend on the system property.

--- auth.business/src/main/java/com/github/pplociennik/auth/business/shared/system/SystemProperty.java
+++ auth.business/src/main/java/com/github/pplociennik/auth/business/shared/system/SystemProperty.java
@@ -1,20 +1,25 @@
 package com.github.pplociennik.auth.business.shared.system;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * An enum holding a name of the system properties.
  *
  * @author Created by: Pplociennik at 20.07.2022 23:20
  */
-@AllArgsConstructor
 @Getter
 public enum SystemProperty {
 
     // -- Custom global properties.
 
-    GLOBAL_CLIENT_URL( "spring.global.client.url" ),
+    GLOBAL_CLIENT_URL( "pp.auth.global.client.url" ),
+
+    GLOBAL_EMAILS_SENDING( "pp.auth.global.emails.sending", "true", "false" ),
 
     // -- Mailing properties.
 
@@ -25,4 +30,19 @@ public enum SystemProperty {
     SYSTEM_JPA_TIME_ZONE( "spring.jpa.properties.hibernate.jdbc.time_zone" );
 
     private final String name;
+    private final Set< String > possibleValues;
+
+    SystemProperty( String aName, String... aPossibleValues ) {
+        name = aName;
+        possibleValues = getAsSet( aPossibleValues );
+    }
+
+    // ### Private helper methods ###
+
+    private static Set< String > getAsSet( String[] aPossibleValues ) {
+        return Stream
+                .of( aPossibleValues )
+                .filter( Objects::nonNull )
+                .collect( Collectors.toSet() );
+    }
 }
