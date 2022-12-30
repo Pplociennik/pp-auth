d27a19494bd360f3e1b55c26579c5ab22156683f
pplociennik <pplociennik097@gmail.com>
2022-12-30 02:46:19 +0100

    [NO_JIRA] Moved and renamed LocalizedApplicationEvent.

--- auth.business/src/main/java/com/github/pplociennik/auth/business/shared/events/OnAccountConfirmationCompleteEvent.java
+++ auth.business/src/main/java/com/github/pplociennik/auth/business/shared/events/OnAccountConfirmationCompleteEvent.java
@@ -1,5 +1,6 @@
 package com.github.pplociennik.auth.business.shared.events;
 
+import com.github.pplociennik.commons.events.BaseLocalizedApplicationEvent;
 import lombok.Getter;
 
 import java.util.Locale;
@@ -10,14 +11,10 @@ import java.util.Locale;
  * @author Created by: Pplociennik at 06.08.2022 22:33
  */
 @Getter
-public class OnAccountConfirmationCompleteEvent extends LocalizedApplicationEvent {
+public class OnAccountConfirmationCompleteEvent extends BaseLocalizedApplicationEvent {
 
     public OnAccountConfirmationCompleteEvent( Object source, Locale aLocale ) {
         super( source, aLocale );
-    }
-
-    public Locale getLocale() {
-        return locale;
     }
 
 }
--- auth.business/src/main/java/com/github/pplociennik/auth/business/shared/events/OnRegistrationCompleteEvent.java
+++ auth.business/src/main/java/com/github/pplociennik/auth/business/shared/events/OnRegistrationCompleteEvent.java
@@ -1,5 +1,7 @@
 package com.github.pplociennik.auth.business.shared.events;
 
+import com.github.pplociennik.commons.events.BaseLocalizedApplicationEvent;
+import lombok.Getter;
 import org.springframework.lang.NonNull;
 
 import java.util.Locale;
@@ -9,12 +11,9 @@ import java.util.Locale;
  *
  * @author Created by: Pplociennik at 01.07.2022 12:13
  */
-public class OnRegistrationCompleteEvent extends LocalizedApplicationEvent {
+@Getter
+public class OnRegistrationCompleteEvent extends BaseLocalizedApplicationEvent {
     public OnRegistrationCompleteEvent( @NonNull Object source, @NonNull Locale aLocale ) {
         super( source, aLocale );
-    }
-
-    public Locale getLocale() {
-        return locale;
     }
 }
